9aa30222cb53028d5f1235e161d4713129054a24
eyder <eydertinoco@outlook.com>
2024-06-19 07:52:05 -0300

    Listagem dos pacientes

--- src/main/java/med/voll/api/dto/ListagemPacientes.java
+++ src/main/java/med/voll/api/dto/ListagemPacientes.java
@@ -1,24 +1,24 @@
 package med.voll.api.dto;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import med.voll.api.enums.Especialidade;
+import med.voll.api.jpa.MedicoJPA;
+import med.voll.api.jpa.PacienteJPA;
 
-public record CadastrarPaciente(
+public record ListagemPacientes(
         @NotBlank
         String nome,
         @NotBlank
         @Email
         String email,
         @NotBlank
-        String telefone,
-        @NotBlank
         @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
-        String cpf,
-        @NotNull
-        @Valid
-        CadastrarEndereco endereco
+        String cpf
 ) {
+    public ListagemPacientes(PacienteJPA paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+    }
 }
